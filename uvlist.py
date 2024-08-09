3e3fc48c8beec5aba50af5844dc54bb83fa91e10
Joshua Young <Joshuareiyoung@gmail.com>
2024-08-08 21:04:17 -0400

    uvlist.py

--- uvlist.py
+++ uvlist.py
@@ -2,6 +2,14 @@ from bs4 import BeautifulSoup
 import pandas
 import requests
 import time
+import sqlite3
+import urllib.error
+import ssl
+from urllib.parse import urljoin
+from urllib.parse import urlparse
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import collections
 
 #Get website data
 def get_game_data(url):
