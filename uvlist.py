c71721432dcc2a973290096a0d256674516158c8
Joshua Young <Joshuareiyoung@gmail.com>
2024-08-13 21:58:25 -0400

    uvlist.py

--- uvlist.py
+++ uvlist.py
@@ -1,12 +1,8 @@
 from bs4 import BeautifulSoup
-import pandas
 import requests
-import sqlite3
 
-page_to_scrape = requests.get('https://www.uvlist.net/gamesearch/')
-soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
-uv_row = soup.find_all('tr')
-
-for row in uv_row:
-    cells = uv_row.find_all('td')
-    cell_text = [cell.get_text(strip=True) for cell in cells]
+url = 'https://www.uvlist.not/gamesearch/'
+response = requests.get(url)
+html_content = response.text
+soup = BeautifulSoup(html_content, 'html.parser')
+table = soup.find(#table, {class: table-class-name})
